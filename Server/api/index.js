1c95a08438c942c62a60088b37023b707d7378fc
saahil0708 <saahil.ind.dgp@gmail.com>
2026-04-09 14:34:17 +0530

    ..

--- Server/api/index.js
+++
@@ -1,8 +0,0 @@
-import app from '../src/app.js';
-import connectDB from '../src/Config/DB/Db.js';
-import config from '../src/Config/Configs.js';
-
-// Connect Database
-connectDB();
-
-export default app;
--- Server/src/app.js
+++ Server/src/app.js
@@ -6,7 +6,7 @@ import chatbotRoutes from './Routes/chatbotRoutes.js';
 const app = express();
 
 app.use(cors({
-    origin: process.env.CLIENT_URL || ['http://localhost:5173', 'https://neuro-sync.vercel.app', 'https://neuro-backend.vercel.app'],
+    origin: 'http://localhost:5173',
     methods: ['GET', 'POST', 'PUT', 'DELETE', 'OPTIONS'],
     allowedHeaders: ['Content-Type', 'Authorization'],
     credentials: true
